4bb97b624183567f7a6035ce02b7790f600714d4
st2f <polatsik@gmail.com>
2024-02-26 21:55:41 +0100

    php extensions

---
+++ html/mysqli.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "db";
+$username = "user";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
